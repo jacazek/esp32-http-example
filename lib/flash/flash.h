89f2d31b8871e053c4de59de56f848a108a9ad01
jacob <dukob86@yahoo.com>
2023-07-13 19:29:23 -0400

    feature: use arduino framework

--- lib/flash/flash.h
+++
@@ -1,5 +0,0 @@
-#ifndef _EXAMPLE_FLASH_H_
-#define _EXAMPLE_FLASH_H_
-
-void flash_init();
-#endif
--- lib/wifi/wifi.h
+++
@@ -1,7 +0,0 @@
-#ifndef _EXAMPLE_WIFI_H
-#define _EXAMPLE_WIFI_H
-
-void wifi_init(void);
-void wifi_scan(void);
-
-#endif
