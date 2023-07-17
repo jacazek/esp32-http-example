ab3a22e6b26c9da6508972057a1a898a8caea917
jacob <dukob86@yahoo.com>
2023-07-16 23:37:42 -0400

    feat: retry device check

--- scripts/device_enumeration.sh
+++ scripts/device_enumeration.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
 echo "Unplug the device, then press enter to continue"
 read keyvalue
+sleep 2
 BEFORE=$(ls /dev/tty*)
-# echo $BEFORE
 
 echo "Plug the device into any port then press enter to continue"
 read keyvalue
-echo "Device enumeration can take a second and I know everyone is impatient, so I tricked you into reading this message while enumeration occurs.\
-If you press enter now, the device should have finished enumeration."
-read keyvalue
-AFTER=$(ls /dev/tty*)
-# echo $AFTER
+
+echo -n "Checking for device to enumerate.."
+attempts=5
+count=0
+while  [ $count -lt $attempts ] && [[ -z $RESULT ]]
+do
+    ((count=count+1))
+    echo -n "."
+    AFTER=$(ls /dev/tty*)
+    RESULT=$(diff <(echo "$BEFORE") <(echo "$AFTER") | tail -n 1 | cut -d " " -f2)
+
+    sleep .3
+done
+echo ""
+echo ""
 
 printSeparator() {
     echo "================================================================"
 }
 
-
-RESULT=$(diff <(echo "$BEFORE") <(echo "$AFTER") | tail -n 1 | cut -d " " -f2)
+# RESULT=$(diff <(echo "$BEFORE") <(echo "$AFTER") | tail -n 1 | cut -d " " -f2)
 if [[ ! -z "$RESULT" ]] then
+    sleep 1
     printSeparator
     echo ""
     echo "Today is your lucky day. Device found is: $RESULT"
@@ -27,10 +37,14 @@ if [[ ! -z "$RESULT" ]] then
     echo $(stat -c "Owner - %U:%u; Group - %G:%g" $RESULT)
     echo ""
     printSeparator
+   else
+    printSeparator
+    echo ""
+    echo "Whelp, no new device was detected. Maybe try again?" echo ""
     echo ""
     echo "Make sure you have configured udev rules for linux or installed drivers for windows."
     echo "linux udev - https://docs.platformio.org/en/stable/core/installation/udev-rules.html#platformio-udev-rules"
     echo "windows driver (you probably cannot even run this script if using windows, but here it is anyway) - https://docs.espressif.com/projects/esp-idf/en/v4.3-beta2/esp32s2/api-guides/dfu.html#usb-drivers-windows-only"
-else
-    echo "Whelp, no new device was detected. Maybe try again?"
+    echo ""
+    printSeparator
 fi 
